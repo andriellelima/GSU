2ed635af868eccfc2fa35052a95accda4cb8f498
Zero <savyosilv@gmail.com>
2019-11-11 17:14:45 -0500

    Models do serviço e ajuste dos outros models

--- projeto/gsu/base/models.py
+++ projeto/gsu/base/models.py
@@ -5,23 +5,49 @@ from django.contrib.auth.models import User
 class Setor(models.Model):
     nome = models.CharField("Nome do Setor",max_length=100)
     reponsavel = models.CharField("Nome do Resposávek",max_length=100)
-    descricao = models.CharField("Descrição do(s) serviço(s)",max_length=1000)
-    documento = models.CharField("Documentos necessários",max_length=200)
-    bloco = models.CharField(max_length=50)
-    andar = models.CharField(max_length=30)
-    sala = models.CharField(max_length=50)
+    bloco = models.CharField('Bloco', max_length=50)
+    andar = models.CharField('Andar',max_length=30)
+    sala = models.CharField('Sala',max_length=50)
     horario = models.CharField("horário de atendimento",max_length=30)
     telefone = models.CharField(max_length=10)
     email = models.CharField(max_length=100)
 
+    class Meta:
+        verbose_name = 'Setor'
+        verbose_name_plural = 'Setores'
+        ordering = ['nome']
+
     def __str__(self):
         return self.nome
+
+
+class Servico(models.Model):
+    titulo = models.CharField(max_length=100)
+    setor = models.ForeignKey(Setor, on_delete=models.CASCADE)
+    descricao = models.TextField('Descrição', blank=False)
+    documentos = models.CharField('Documentos')
+
+    class Meta:
+        verbose_name = 'Serviço'
+        verbose_name_plural = 'Serviços'
+        ordering = ['titulo']
+
+    def __str__(self):
+        return self.titulo    
+
 
 class Usuario(models.Model):
     usuario = models.ForeignKey(User, null=True, blank=True, editable=True,  related_name='usuario_set', on_delete=models.CASCADE)
-    nome = models.CharField(max_length=100)
-    # campus = models.CharField(max_length=100,choices=CAMPUS_CHOICES)
     setor = models.ForeignKey(Setor, null=True, blank=True,on_delete=models.CASCADE)
+    cpf = models.CharField('CPF', max_length=11)
+    matricula = models.CharField('Matrícula', max_length=15)
+    funcao = models.CharField('Função', max_length=50)
+    # nome = models.CharField(max_length=100)
+    # campus = models.CharField(max_length=100,choices=CAMPUS_CHOICES)
 
+    class Meta:
+        verbose_name = 'Usuario'
+        verbose_name_plural = 'Usuários'
+        
     def __str__(self):
-        return self.nome
+        return self.user.name
