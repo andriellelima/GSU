e1390104ce223a1a6e19e12ca0384962237cad0d
ShibaT20 <55547863+ShibaT20@users.noreply.github.com>
2021-01-25 15:42:05 -0400

    Update admin.py

--- projeto/gsu/base/admin.py
+++ projeto/gsu/base/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from .models import Sugestao, Usuario
 
 
-@admin.register(Usuario)
-class UsuarioAdmin(admin.ModelAdmin):
-    	list_display = (
-		'__str__',
-	)
+# @admin.register(Usuario)
+# class UsuarioAdmin(admin.ModelAdmin):
+#     	list_display = (
+# 		'__str__',
+# 	)
 
 @admin.register(Sugestao)
 class SugestaoAdmin(admin.ModelAdmin):
